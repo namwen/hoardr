0a0cb55e05872bd7883ac5fd3b8ff2b97a810bad
Will Newman <wnewman88@gmail.com>
2014-07-24 22:05:00 -0500

    encryption and decryption of files mostly working

--- hoardr.rb
+++ hoardr.rb
@@ -2,6 +2,8 @@ require 'digest'
 require 'zip'
 require 'flickraw'
 require 'open-uri'
+require 'gibberish'
+require 'shellwords'
 
 class Hoardr 
 	attr_accessor :flickr_api_key
@@ -33,7 +35,7 @@ class Hoardr
 		else
 			token = flickr.get_request_token
 			auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
-			puts "navigate to this url to authorize: "
+			puts "Navigate to this url to authorize your account: "
 			puts auth_url
 			puts "enter confirmation #: "
 			verify = gets.strip
@@ -44,22 +46,21 @@ class Hoardr
 	# build hash of filenames mapped to their size in megabytes
 	def build_filesizes
 		Dir.foreach(@source_path) do |file|
-			next if file == '.' || file == '..'
+			next if file == '.' || file == '..' || file == '.DS_Store'
 			filesize = File.size("#{@source_path}/#{file}").to_f / 2**20
-			next if filesize > @MAX_SIZE
+			next if filesize > @MAX_SIZE 
 			@filesizes[file] = filesize
 		end
 	end
 	
-	# returns 200mb block of files
-	# array of files whose cumulative sizes are under the max_upload_size
+	# returns array of available files whose cumulative sizes are under the max_upload_size
 	def accumulate_files
 		files = []
 		temp_size = 0
 		not_done = true
 		while temp_size <= @MAX_SIZE && not_done
 			@filesizes.each do |key,value| 
-				next if temp_size+value > @MAX_SIZE
+				next if temp_size+value > @MAX_SIZE 
 				temp_size+=value
 				files << key 
 				@filesizes.delete(key)
@@ -68,20 +69,24 @@ class Hoardr
 		end
 		files
 	end
-
+	
 	# pass in files to be archived ( already filtered by size )
-	# return success or failure of archive creation
 	def create_archive(files)
-		File.delete(@destination_path+'/'+ARCHIVE_FILE) if File.exists?(@destination_path+'/'+ARCHIVE_FILE)
-		Zip::File.open(@destination_path+'/'+ARCHIVE_FILE, Zip::File::CREATE) do |zipfile|
-			files.each do |file|
-				zipfile.add(file, @source_path+'/'+file)
+		puts "Enter your encryption key: "
+		key = gets.chomp
+		cipher = Gibberish::AES.new(key)
+		File.delete(@destination_path+ARCHIVE_FILE) if File.exists?(@destination_path+'/'+ARCHIVE_FILE)
+		Zip::File.open(@destination_path+ARCHIVE_FILE, Zip::File::CREATE) do |zipfile|
+			Dir.glob(@source_path+'**/*').each do |file|
+				next if File.directory?(file)
+				cipher.encrypt_file(file, file+'.enc')
+				zipfile.add(file.sub(@source_path,'')+'.enc',file+'.enc')
 			end
 		end
 	end
-	
+
 	# concatenates the archive file and gif to form the combined gif
-	# return name of combined file for use later
+	# return name of combined file
 	def build_backup
 		combined_file = Time.now.to_i.to_s
 		system("cat #{@gif_path} #{@destination_path}/#{ARCHIVE_FILE} > #{@destination_path}/#{combined_file}.gif")
@@ -89,7 +94,7 @@ class Hoardr
 	end
 	
 	# takes the combined image and uploads it to flickr
-	# returns the id of the uploaded image, to be used for data logging
+	# returns the id of the uploaded image
 	def do_upload(combined_image, title='stuffr-backup', tags)
 		begin
 			return flickr.upload_photo(combined_image, :title => title, :tags => tags, :is_public => 0)
@@ -107,31 +112,111 @@ class Hoardr
 			combined_file = build_backup
 			hashed_filenames = files_to_backup.map{|file| Digest::SHA1.hexdigest(file)}
 			archive_id = do_upload(@destination_path+'/'+combined_file+'.gif',combined_file, hashed_filenames.join(" ") )
+			system("rm -rf #{@destination_path}/#{combined_file}.gif")
 			log_archive(archive_id, hashed_filenames, files_to_backup)
 			ids << archive_id
 			files_to_backup = accumulate_files
 		end
+		system("rm -rf #{@destination_path}/#{ARCHIVE_FILE}")
 		puts "uploads complete"
 		puts ids
 	end
 
 	def log_archive(archive_id, hashed_filenames, raw_filenames)
 		File.open('hoardr-archive.txt', 'a') do |archive_log|
-			archive_log.write("archive-#{archive_id}-#{hashed_filenames.length}\n")
 			hashed_filenames.each_with_index do |f, i|
-				archive_log.write('hash: '+f+' name: '+raw_filenames[i]+"\n")
+				archive_log.write( Marshal.dump(ArchivedFile.new(raw_filenames[i],f, archive_id)))
+				archive_log.write("\n")
 			end
-			archive_log.write("\n")
+		end
+	end
+	
+	def read_archives
+		return unless File.exists?('hoardr-archive.txt')
+		File.open('hoardr-archive.txt', 'r').each do |line|
+			archived_file = Marshal.load(line)
+			puts archived_file.inspect
 		end
 	end
 
-	def download_archive(archive_id)
+	def archived_files
+		return unless File.exists?('hoardr-archive.txt')
+		File.open('hoardr-archive.txt', 'r').each do |line|
+			archived_file = Marshal.load(line)
+			puts archived_file.filename
+		end
+	end
+
+	# retrieves an entire archive folder 
+	def download_raw_archive(archive_id)
 		info = flickr.photos.getInfo(:photo_id => archive_id)
 		photo_url = FlickRaw.url_o(info)
 		open("#{archive_id}.gif", 'wb') do |file|
 			file << open(photo_url).read
 		end
-		system("mv #{archive_id}.gif #{archive_id}.zip | unzip #{archive_id}.zip -d #{archive_id}")
+		system("mv #{archive_id}.gif #{archive_id}.zip | unzip #{archive_id}.zip -d #{@destination_path}#{archive_id}")
+		system("rm -rf #{archive_id}.zip")
+	end
+	
+	def retrieve_archive(archive_id)
+		download_raw_archive(archive_id)
+		puts "Enter your decryption key: "
+		key = gets.chomp
+		cipher = Gibberish::AES.new(key)
+		Dir.glob("#{@destination_path}#{archive_id}/**/*").each do |file|
+			next if File.directory?(file)
+			cipher.decrypt_file(file, file.sub('.enc', ''))
+			system("rm %s" % Shellwords.escape(file))
+		end
+	end
+	
+	# retrieves a single archived file
+	def download_raw_file(filename)
+		return unless file_is_archived?(filename)
+		archive_id = get_file_archive_id(filename)
+		download_archive(archive_id)
+		if( File.directory?(@destination_path+"downloaded_files/"))
+			system("cp #{archive_id}/#{filename} ../downloaded_files")
+		else
+			system("mkdir #{destination_path}downloaded_files | cp #{archive_id}/#{filename} #{@destination_path}downloaded_files")
+		end
 	end
 
+	def retrieve_archived_file(filename)
+		download_raw_file(filename)
+		puts "Enter your decryption key: "
+		key = gets.chomp
+		cipher = Gibberish::AES.new(key)
+		cipher.decrypt_file("#{@destination_path}downloaded_files/#{filename}.enc", filename)	
+		system("rm -rf #{archive_id}")
+		system("rm %s" % Shellwords.escape("#{filename}.enc"))
+	end
+	
+	def find_file_on_flickr(filename)
+		flickr.photos.search(:tags => Digest::SHA1.hexdigest(filename), :user_id => "me")
+	end
+
+	def all_hashed_filenames
+		flickr.tags.getListUser["tags"].inspect
+	end
+	
+	def file_is_archived?(filename)
+		open('hoardr-archive.txt'){ |f| return f.grep(/#{filename}/).size > 0 }
+	end
+	
+	def get_file_archive_id(filename)
+		return unless file_is_archived?(filename)
+		file = nil
+		open('hoardr-archive.txt'){ |f|  file = Marshal.load(f.grep(/#{filename}/)[0]) }
+		file.archive_id
+	end
+end
+
+class ArchivedFile
+	attr_accessor :filename, :hashed_filename, :archive_id
+	def initialize(filename, hashed_filename, archive_id )
+		@filename = filename
+		@hashed_filename = hashed_filename
+		@archive_id = archive_id
+	end
 end
